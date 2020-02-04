13b9cb81d18a59081cf6bd5ef00a0be947dab705
Saqib Dhuka <saqibdhuka@gmail.com>
2020-02-03 23:12:54 -0800

    Working on Drop down list for the layout options

--- app/src/main/java/app/kobetribute/Options.java
+++ app/src/main/java/app/kobetribute/Options.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Options extends AppCompatActivity {
 
@@ -19,7 +21,16 @@ public class Options extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+        //From https://www.youtube.com/watch?v=urQp7KsQhW8
+        Spinner dropDpown = (Spinner) findViewById(R.id.layoutDropDown);
+
+        ArrayAdapter<String> ddAdapter =  new ArrayAdapter<String>(Options.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.layout));
+
+        ddAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        dropDpown.setAdapter(ddAdapter);
+
     }
 
 }

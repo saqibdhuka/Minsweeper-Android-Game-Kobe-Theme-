e904b9d201f0e17f27bec174da99c020b38e1ee0
Saqib Dhuka <saqibdhuka@gmail.com>
2020-02-11 13:34:11 -0800

    Issiue: "Welcome Screen" Solved. Welcome Screen Added. Feature Ready

---
+++ app/src/test/java/app/model/BoardTest.java
@@ -0,0 +1,56 @@
+package app.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    void getMineCount() {
+    }
+
+    @Test
+    void setMineCount() {
+    }
+
+    @Test
+    void getRow() {
+    }
+
+    @Test
+    void setRow() {
+    }
+
+    @Test
+    void getColumn() {
+    }
+
+    @Test
+    void setColumn() {
+    }
+
+    @Test
+    void getBoardArr() {
+    }
+
+    @Test
+    void getBoardArrayVal() {
+    }
+
+    @Test
+    void setBoardArrayVal() {
+    }
+
+    @Test
+    void setBoardArr() {
+    }
+
+    @Test
+    void setupboard() {
+    }
+
+    @Test
+    void assignBoard() {
+    }
+}
